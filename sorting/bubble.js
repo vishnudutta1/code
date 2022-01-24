205558e45113f4e7f358f6570c41a88176955ae6
Vishnu Dutta <92321047+vishnudutta1@users.noreply.github.com>
2022-01-24 09:06:43 +0530

    mac

---
+++ sorting/bubble.js
@@ -0,0 +1,13 @@
+var arr = [10,14,2,11,6]
+
+for(var i = 0;i <arr.length;i++){
+    for(var j = 0;j<arr.length-i-1;j++){
+        if(arr[j]>arr[j+1]){
+            var temp = arr[j];
+            arr[j] = arr[j+1];
+            arr[j+1] = temp;
+        }
+    }
+}
+
+console.log(arr)
